3fbaec895802cc73cbe98d56024f94b1f2a606b0
Yiğit Erdoğan Şafak <erent4sci@gmail.com>
2025-06-23 19:45:50 +0300

    feat(profile): added screen ui draft

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,7 +1,8 @@
+import EventList from "@/components/EventList";
 import Header from "@/components/Header";
 import { COLORS } from "@/constants/colors";
 import { LinearGradient } from "expo-linear-gradient";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function ProfilePage() {
   return (
@@ -20,6 +21,28 @@ export default function ProfilePage() {
         style={StyleSheet.absoluteFill}
       />
       <Header title="Profile" />
+      <View style={{ padding: 20, gap: 10 }}>
+        <Text
+          style={{
+            fontSize: 24,
+            fontWeight: "600",
+            color: "#fff",
+            textAlign: "left",
+          }}
+        >
+          Welcome, Eren 🤩
+        </Text>
+        <Text
+          style={{
+            color: COLORS.main[800],
+          }}
+        >
+          You can view and manage your events here:
+        </Text>
+      </View>
+      <View style={{ paddingHorizontal: 20 }}>
+        <EventList events={require("@/assets/data/events.json")} />
+      </View>
     </SafeAreaView>
   );
 }
